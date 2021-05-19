f4e0391978b84f13d8b9099244ca11199bb69ae7
Ulianoff <umaxim2000@mail.ru>
2021-05-19 22:03:56 +0300

    Initial commit

---
+++ src/main/java/com/ulianoff/quizapplication/security/quizapplicationsecurity/QuizApplicationSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.ulianoff.quizapplication.security.quizapplicationsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizApplicationSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizApplicationSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ulianoff/quizapplication/security/quizapplicationsecurity/QuizApplicationSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulianoff.quizapplication.security.quizapplicationsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApplicationSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
